6b602d9fbf71cd5f770498a78272a205a106ad94
Ede Zhong <eden.zhong@gmail.com>
2014-01-15 11:30:51 +0800

    Create

---
+++ pcreator.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:: check parameters
+if "%1" == "" goto usage
+if "%2" == "" goto usage
+
+::copy prototype dir
+xcopy prototype %2\%1 /E /I
+
+::create windows update.bat
+echo cmake -DCMAKE_BUILD_TYPE=Debug -DPRJ_NAME=%1 ..\src>%2\%1\debug\update.bat
+echo cmake -DCMAKE_BUILD_TYPE=Release -DPRJ_NAME=%1 ..\src>%2\%1\release\update.bat
+::create linux update 
+echo cmake -DCMAKE_BUILD_TYPE=Debug -DPRJ_NAME=%1 ../src>%2\%1\debug\update
+echo cmake -DCMAKE_BUILD_TYPE=Release -DPRJ_NAME=%1 ../src>%2\%1\release\update
+
+goto end
+
+:usage
+echo usage: pcreator [project_name] [path]
+
+:end
---
+++ prototype/config/macconf.cmake
@@ -0,0 +1,11 @@
+set(EXTRA_INCLUDES 
+	""
+	)
+set(EXTRA_LIBS
+	""
+	)
+
+set(REQUIRED_LIBRARIES
+#     gmock
+   )
+#   link_directories(${EXTRA_LIBS} ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
